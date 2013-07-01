fda757160773d76ecfd2de820c68ffca9124d142
Matt Tschoegl <matt.tschoegl@gmail.com>
2013-07-01 13:45:57 -0500

    minor changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NickBlog::Application.config.secret_token = '10ab08b3a71a0b11000cee7b7a4d6c61d1cf8e882e4e14cb3bbc8211005e94aff0e8e3de808bb6e1ad0a9dfbf5825214893f06187557e85b132d697941c6577a'
